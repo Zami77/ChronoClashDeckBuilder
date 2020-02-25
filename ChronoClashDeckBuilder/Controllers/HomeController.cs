17d19c2da74b41e888be6270041aa1e95a10d3e9
Daniel Lungaro <chunk_77@hotmail.com>
2020-02-25 13:07:23 -0800

    Some updates to web pages physical look, and images added.

--- ChronoClashDeckBuilder/Controllers/HomeController.cs
+++ ChronoClashDeckBuilder/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ChronoClashDeckBuilder.Controllers
             return View();
         }
 
+        public IActionResult CardDatabase()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- ChronoClashDeckBuilder/Program.cs
+++ ChronoClashDeckBuilder/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +14,7 @@ namespace ChronoClashDeckBuilder
     {
         public static void Main(string[] args)
         {
+
             CreateHostBuilder(args).Build().Run();
         }
 
