04db93201e0c2105c184bcc42d20aee915d0f69a
Daniel Lungaro <chunk_77@hotmail.com>
2020-03-08 10:50:21 -0700

    added Razor CRUD files, but currently not displaying Card list.

---
+++ ChronoClashDeckBuilder/Views/Cards/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ChronoClashDeckBuilder.Models;
+
+namespace ChronoClashDeckBuilder
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ChronoClashDeckBuilder.Models.ChronoClashDbContext _context;
+
+        public DeleteModel(ChronoClashDeckBuilder.Models.ChronoClashDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Card Card { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Card = await _context.Cards.FirstOrDefaultAsync(m => m.CardNumber == id);
+
+            if (Card == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Card = await _context.Cards.FindAsync(id);
+
+            if (Card != null)
+            {
+                _context.Cards.Remove(Card);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
