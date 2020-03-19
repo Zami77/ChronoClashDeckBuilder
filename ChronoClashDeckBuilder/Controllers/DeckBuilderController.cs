9edfa81ab1034d7737ea17957c56036482d110b2
Daniel Lungaro <chunk_77@hotmail.com>
2020-03-18 21:44:43 -0700

    Added Deck Builder page and all card images to it.

---
+++ ChronoClashDeckBuilder/Controllers/DeckBuilderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChronoClashDeckBuilder.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChronoClashDeckBuilder.Controllers
+{
+    public class DeckBuilderController : Controller
+    {
+        private ICardRepository repository;
+        public DeckBuilderController(ICardRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index()
+        {
+            return View(repository.Cards);
+        }
+    }
+}
