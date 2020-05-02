300e2a92ad4a206a66116e282e3944e9e08ce8ae
Daniel Lungaro <chunk_77@hotmail.com>
2020-05-01 19:47:42 -0700

    Added Filters to Deck Builder page

--- ChronoClashDeckBuilder/Controllers/DeckBuilderController.cs
+++ ChronoClashDeckBuilder/Controllers/DeckBuilderController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using ChronoClashDeckBuilder.Models;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace ChronoClashDeckBuilder.Controllers
 {
@@ -11,27 +11,48 @@ namespace ChronoClashDeckBuilder.Controllers
     {
         private ICardRepository repository;
         List<Card> curDeck = new List<Card>();
+
         public DeckBuilderController(ICardRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index()
+        //Look at using cookies
+        [HttpGet]
+        public IActionResult Index(string cardNumber, string cardName, string cardColor, string cardAbility)
         {
-            //if (cardNumber != null)
-            //    curDeck.Add(repository.GetCard(cardNumber));
+            if (cardNumber != null)
+                curDeck.Add(repository.GetCard(cardNumber));
+            ViewData["CardNameFilter"] = cardName;
+            ViewData["CardColorFilter"] = cardColor;
+            ViewData["CardAbilityFilter"] = cardAbility;
 
+            var cards = repository.Cards;
+            if (!String.IsNullOrEmpty(cardName))
+            {
+                cards = cards.Where(c => c.CardName.Contains(cardName));
+
+            }
+            if (!String.IsNullOrEmpty(cardColor))
+            {
+                cards = cards.Where(c => c.CardColor.Contains(cardColor));
+
+            }
+            if (!String.IsNullOrEmpty(cardAbility))
+            {
+                cards = cards.Where(c => c.CardAbilities.Contains(cardAbility));
+
+            }
             return View(new Models.ViewModels.DeckBuilderListViewModel
             {
-                Cards = repository.Cards,
+                Cards = cards.OrderBy(c => c.CardNumber),
                 NewDeck = curDeck
-            });
+            }) ;
         }
 
-        [HttpPost]
-        public void AddCard(String cardNum)
+        public RedirectToActionResult AddToDeck(string cardNumber)
         {
-            curDeck.Add(repository.GetCard(cardNum));
-            RedirectToAction("Index");
+            curDeck.Add(repository.GetCard(cardNumber));
+            return RedirectToAction("Index");
         }
     }
 }
